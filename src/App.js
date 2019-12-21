f313b5dde54bb9414ee0360f82bd66c8a5960827
Zachary Taylor <zachary.taylor@gmail.com>
2019-12-21 14:34:31 -0800

    change text on front page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world. I was changed.</p>
+        <p>Hello world. I was changed. Now I have some resources.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
